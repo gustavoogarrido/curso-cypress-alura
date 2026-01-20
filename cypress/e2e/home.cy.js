b0ec18cc6f1ddf4997636af89670992d30995f51
Gustavo <gustavoog05@hotmail.com>
2026-01-20 13:02:01 -0300

    Using stubs to simulate API responses

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,6 +2,7 @@ describe('Visiting home page', () => {
     it('Should load the home page', () => {
         cy.visit('https://adopet-frontend-cypress.vercel.app/');
         cy.get('.button').click();
+        cy.get('.card').should('not.be.empty');
         cy.get(':nth-child(1) > .card__contact').click()
     })
 })
--- cypress/e2e/login-failure.cy.js
+++ cypress/e2e/login-failure.cy.js
@@ -1,7 +1,10 @@
 describe('Login function', () => {
   beforeEach(() => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.visit('https://adopet-frontend-cypress.vercel.app');
     cy.get('[data-test="login-button"').click();
+    cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
+      statusCode:400,
+    }).as('stubPost')
   })
 
   it('Should display two errors', () => {
@@ -20,5 +23,11 @@ describe('Login function', () => {
     cy.contains(
         'A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres'
     ).should('be.visible')
-  })
+  });
+
+  it('Should fail even though all fields are filled', () => {
+    cy.login('gustavo2@exemplo.com', '@Gustavo123');
+    cy.wait('@stubPost');
+    cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible')
+  });
 })
